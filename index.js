b814829c1d4a1934f2616dd633ba4771b36cc414
kissubot-login <mistagray949@gmail.com>
2025-05-06 23:14:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const verifyTelegramAuth = require('./utils/verifyTelegramAuth.js');
+const verifyTelegramAuth = require('./utils/verifyTelegramAuth');
 const User = require('./models/User');
 const profileRoutes = require('./profileRoutes');
 
