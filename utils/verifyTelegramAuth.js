7712c72b237351d817bfa0a754f5cde404842861
kissubot-login <mistagray949@gmail.com>
2025-05-06 22:51:25 +0100

    Update verifyTelegramAuth.js

--- utils/verifyTelegramAuth.js
+++ utils/verifyTelegramAuth.js
@@ -1,1 +1,11 @@
+const crypto = require('crypto');
 
+function checkTelegramAuth(data, botToken) {
+  const { hash, ...rest } = data;
+  const secret = crypto.createHash('sha256').update(botToken).digest();
+  const checkString = Object.keys(rest).sort().map(k => ${k}=${rest[k]}).join('\n');
+  const hmac = crypto.createHmac('sha256', secret).update(checkString).digest('hex');
+  return hmac === hash;
+}
+
+module.exports = checkTelegramAuth;
