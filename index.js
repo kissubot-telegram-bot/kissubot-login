f6e5cb6c5bbaafbfb77e5cdc2c036305ef1cd910
kissubot-login <mistagray949@gmail.com>
2025-05-29 22:33:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json())
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
   useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB error:', err))
+})
 
 app.post('/auth', async (req, res) => {
   const userData = req.body
